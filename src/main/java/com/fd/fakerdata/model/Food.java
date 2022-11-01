2fe82f37f9f2aac3c3775a3e49915af7e303b614
hujiaoxiang <hujiaoxiang@qq.com>
2022-11-01 08:50:00 +0800

    添加Food

---
+++ src/main/java/com/fd/fakerdata/model/Food.java
@@ -0,0 +1,20 @@
+package com.fd.fakerdata.model;
+
+import com.fd.fakerdata.tool.Faker;
+
+/**
+ * @ClassName Food
+ * @Description TODO
+ * @Author hujiaoxiang
+ * @Date 2022/11/1
+ * @Version 1.0
+ */
+public class Food extends BaseModel {
+    public Food(Faker faker) {
+        super(faker);
+    }
+
+    public void getFoodName() {
+
+    }
+}
